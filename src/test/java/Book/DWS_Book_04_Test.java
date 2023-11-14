0f4af945df4ccaf8cda5365c449456876cbb731f
BhargaviATP <yadati.bhargavi@gmail.com>
2023-11-14 20:29:32 +0530

    Just add space in log for DWS_Book_04_Test

--- src/test/java/Book/DWS_Book_04_Test.java
+++ src/test/java/Book/DWS_Book_04_Test.java
@@ -18,25 +18,24 @@ public class DWS_Book_04_Test extends BaseClass {
 	@Test(groups="System Group")
 	public void addBookToCart() throws InterruptedException {
 		bookpage.getBooks().click();
-		logger.log(Status.INFO, "User Clicked on Books Field");
+		logger.log(Status.INFO, "User Clicked on Books Field ");
 		Thread.sleep(2000);
 
 		bookpage.getAddBookstoCart().click();
-		logger.log(Status.INFO, "User clicked on addBookToCart");
+		logger.log(Status.INFO, "User clicked on addBookToCart ");
 		Thread.sleep(2000);
 
 		cartpage.getShoppingcart().click();
-		logger.log(Status.INFO, "User clicked on Shopping Cart");
+		logger.log(Status.INFO, "User clicked on Shopping Cart ");
 
 		assertTrue(bookpage.getBookinCart().isDisplayed());
-		logger.log(Status.INFO, "Book added to Shopping Cart");
+		logger.log(Status.INFO, "Book added to Shopping Cart ");
 
 		bookpage.getRemoveCart().click();
 		bookpage.getUpdateCart().click();
-		logger.log(Status.INFO, "Book removed from Shopping Cart");
+		logger.log(Status.INFO, "Book removed from Shopping Cart ");
 		
-		WebDriverWait wait= new WebDriverWait(driver,10);
-		wait.until(ExpectedConditions.visibilityOf(bookpage.getCartisEmptyMSG()));
+		WBUtility.explicitWaitReference(10).until(ExpectedConditions.visibilityOf(bookpage.getCartisEmptyMSG()));
 	}
 }
 
