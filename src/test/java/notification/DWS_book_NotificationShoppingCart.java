1d710a89c37e2f0a5797ce55feefff9b943731a7
BhargaviATP <yadati.bhargavi@gmail.com>
2023-11-15 21:08:54 +0530

    package changed

--- src/test/java/notification/DWS_book_NotificationShoppingCart.java
+++ src/test/java/notification/DWS_book_NotificationShoppingCart.java
@@ -1,13 +1,15 @@
-package basic;
+package notification;
 
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
+
 import genericUtilities.BaseClass;
 
-public class DWS_book_NotificationShoppingCart extends BaseClass{
+public class DWS_book_NotificationShoppingCart extends BaseClass {
 	@Test(groups="System Group")
 	public void goTo_ShoppingCart_Through_Notification_Bar(){
 		
@@ -24,3 +26,5 @@ public class DWS_book_NotificationShoppingCart extends BaseClass{
 		WBUtility.explicitWaitReference(10).until(ExpectedConditions.visibilityOf(bookpage.getRemoveCart()));
 }
 }
+
+
